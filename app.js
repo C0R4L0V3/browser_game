db62a6b8581b6d07d4a538194af6e3a6f0301a26
JenCW <jencortezwalters@gmail.com>
2024-10-24 13:09:06 -0700

    framework completed

---
+++ app.js
@@ -0,0 +1,7 @@
+//steps to setup:
+// Make Box clickable to match color to player turn
+// Clear the board after each game
+// future development change to X's and O's
+// future development for Winner Sign
+
+const boxes = document.querySelectorAll(box)
